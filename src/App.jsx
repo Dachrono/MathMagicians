531e32e07b75c14e6a2fd885ee8dbcf251fcdd3d
Dachrono <bluefire_andy@hotmail.com>
2023-07-03 16:58:17 -0600

    Intall react and linters

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import mathLogo from './mathLogo.gif';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={mathLogo} className="App-logo" alt="logo" />
+        <h1>
+          Math Magicians!
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
