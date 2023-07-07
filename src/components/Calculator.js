e6cc90a500963784e04d8108dba212a6f5b552bc
Dachrono <bluefire_andy@hotmail.com>
2023-07-07 13:22:19 -0600

    add logic to the calculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,13 +1,27 @@
+import { useState } from 'react';
 import './Calculator.css';
 import Buttons from './Buttons';
+import calculate from '../logic/calculate';
 
 function Calculator() {
+  const [calcRes, setOper] = useState({});
+  const handleClick = (butName) => {
+    const result = calculate(calcRes, butName);
+    setOper(result);
+  };
+
   return (
     <div className="calc">
       <div className="result">
-        <span>0</span>
+        <span>
+          { (JSON.stringify(calcRes) === '{}') ? '0' : null }
+          {calcRes.total || ''}
+          {calcRes.operation || ''}
+          {calcRes.next || ''}
+
+        </span>
       </div>
-      <Buttons />
+      <Buttons handleClick={handleClick} />
     </div>
   );
 }
