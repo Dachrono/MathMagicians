0bfbf84461e780fb9586e8399fbb7b856f662bcb
Dachrono <bluefire_andy@hotmail.com>
2023-07-07 18:33:25 -0600

    API connect and show API data

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
 import Calculator from './components/Calculator';
+import Quote from './components/quote';
 
 function App() {
   return (
     <div className="container">
+      <Quote />
       <Calculator />
     </div>
   );
